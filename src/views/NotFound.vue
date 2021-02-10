a9d7a34d5a60887c1389680b814dfdc80a48a2f9
Claudine LAFINE <claudine.lafine@gmail.com>
2021-02-10 11:32:11 +0100

    catch all rule + not found component

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p class="not-found">
+      <span class="number">404</span>Oups! Seems like we've hit a rock...
+    </p>
+  </div>
+</template>
+<style scoped>
+.not-found {
+  margin-top: 2rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+}
+.number {
+  font-size: 10rem;
+}
+</style>
